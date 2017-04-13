31699eb924d40c39c7985d1b60bdfa9a405faa17
Alexander <alykoshin@gmail.com>
2017-04-13 22:13:24 +0300

    convert all errors into AppError

---
+++ demo/demo.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const errors = require('mini-rest-errors');
+const handler = require('../')({});
+
+
+let req = {
+};
+
+let res = {
+  headersSent: false,
+  status: function(status) { /*console.log('*** res.status:', status);*/ return this; },
+  json:   function(json)   { /*console.log('*** res.json:', json);    */ return this; },
+};
+
+let next = function(err, req, res, next) {
+  console.log('*** next(): err:', err);
+};
+
+let err;
+
+err = 'test error string';
+handler(err, req, res, next);
+
+err = new Error('test Error object');
+handler(err, req, res, next);
+
+err = new errors.AppError('test Error object');
+handler(err, req, res, next);
