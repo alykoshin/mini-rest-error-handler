941f4c47a070a213b668bf1718d72622a74190b0
Alexander <alykoshin@gmail.com>
2018-01-08 15:07:29 +0300

    hide error details from http response (default)

--- demo/demo.js
+++ demo/demo.js
@@ -9,8 +9,8 @@ let req = {
 
 let res = {
   headersSent: false,
-  status: function(status) { /*console.log('*** res.status:', status);*/ return this; },
-  json:   function(json)   { /*console.log('*** res.json:', json);    */ return this; },
+  status: function(status) { console.log('*** res.status:', status); return this; },
+  json:   function(json)   { console.log('*** res.json:', json);     return this; },
 };
 
 let next = function(err, req, res, next) {
@@ -25,5 +25,5 @@ handler(err, req, res, next);
 err = new Error('test Error object');
 handler(err, req, res, next);
 
-err = new errors.AppError('test Error object');
+err = new errors.AppError('pageNotFound', 'some-message');
 handler(err, req, res, next);
